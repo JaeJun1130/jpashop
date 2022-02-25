2370abc7cd0f6c6f84ea476144764153e12af640
jaejun1130 <wjswowns1234@gmail.com>
2022-02-25 21:52:36 +0900

    Item 도메인 설계

--- src/main/java/com/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/com/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package com.jpabook.jpashop.domain.item;
 
 import com.jpabook.jpashop.domain.Category;
+import com.jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -28,4 +29,25 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+    //setter 를 사용하기보다는 값이 변경될 일 이 있으면 비즈니스 로직으로 인해 변경하는게 객체지향적 설계.
+
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+
+        if(restStock < 0) throw new NotEnoughStockException("need more stock");
+
+        this.stockQuantity = restStock;
+    }
 }
