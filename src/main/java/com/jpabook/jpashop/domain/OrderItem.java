2bbb2744d44c6fc8408f532b23de2316e117fba8
jaejun1130 <wjswowns1234@gmail.com>
2022-02-27 09:41:55 +0900

    주문,주문상품 엔티티 개발

--- src/main/java/com/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/com/jpabook/jpashop/domain/OrderItem.java
@@ -3,6 +3,7 @@ package com.jpabook.jpashop.domain;
 import com.jpabook.jpashop.domain.item.Item;
 import lombok.Getter;
 import lombok.Setter;
+import org.aspectj.weaver.ast.Or;
 
 import javax.persistence.*;
 
@@ -25,7 +26,28 @@ public class OrderItem {
 
     //주문 가격
     private int orderPrice;
-
     //주문 수량
     private int count;
+
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    //==조회 로직==//
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
